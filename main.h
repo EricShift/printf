2ec1243f11e40567d310d6b05dad2527e7c28e1a
Eric <Ericthabiso55@gmail.com>
2022-08-09 06:34:32 -0700

    Main headers

--- main.h
+++ main.h
@@ -1,10 +1,15 @@
 #ifndef PRINTF_H
 #define PRINTF_H
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <limits.h>
 
 int _printf(const char *format, ...);
+void _strprintf(const char *str);
+void _count(int *ptr, char *str);
+char *_itoa(int num);
 
 #endif
